d1b4ddf7c566ff7376ee5b9f5516d111c0da5f2b
Mayank Pathak <mayankdcoder@gmail.com>
2016-01-11 13:15:08 +0530

    Create Titles.py

---
+++ Titles.py
@@ -0,0 +1,13 @@
+#Draws labels for the axes using matplotlib
+
+import matplotlib.pyplot as plt
+
+
+x = [1, 2, 3]
+y = [5, 7, 4]
+
+plt.plot(x, y)
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.title('This is a Title')
+plt.show()
