80917e2a81acff8bbb16b43834c97800f670d0e9
Mayank Pathak <mayankdcoder@gmail.com>
2016-01-19 17:17:55 +0530

    Create 10 - Geting data from a file(csv).py

---
+++ 10 - Geting data from a file(csv).py
@@ -0,0 +1,19 @@
+# Getting data from a file using csv module
+
+import matplotlib.pyplot as plt
+import csv
+
+
+x = []
+y = []
+
+with open('example.txt', 'r') as csv_file:
+    plots = csv.reader(csv_file, delimiter=',')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(int(row[1]))
+plt.plot(x, y, label='First Graph')
+plt.xlabel('X Axis')
+plt.ylabel('Y Axis')
+plt.title('This is a Title')
+plt.show()
